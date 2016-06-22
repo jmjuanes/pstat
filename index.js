619e81befbd5142f535baea569925eb0d8f7f4d2
jmjuanes <josemijuanes@gmail.com>
2016-06-22 20:52:03 +0200

    index.js: updated tabs

--- index.js
+++ index.js
@@ -4,89 +4,89 @@ var fs = require('fs');
 //pstat class
 var pstat =
 {
-	//Check if path is a file
-	isFile: function(path, callback)
-	{
-		//Get the stat for this path
-		pstat.Stat(path, function(stat)
-		{
-			//Check for error
-			if(stat === false){ return callback(false); }
+  //Check if path is a file
+  isFile: function(path, callback)
+  {
+    //Get the stat for this path
+    pstat.Stat(path, function(stat)
+    {
+      //Check for error
+      if(stat === false){ return callback(false); }
 
-			//Else, return if is a file
-			return callback(stat.isFile());
-		});
-	},
+      //Else, return if is a file
+      return callback(stat.isFile());
+    });
+  },
 
-	//Check if path is a file sync
-	isFileSync: function(path)
-	{
-		//Get the stat for this path
-		var stat = pstat.StatSync(path);
+  //Check if path is a file sync
+  isFileSync: function(path)
+  {
+    //Get the stat for this path
+    var stat = pstat.StatSync(path);
 
-		//Check for error
-		if(stat === false){ return false; }
+    //Check for error
+    if(stat === false){ return false; }
 
-		//Return if is a file
-		return stat.isFile();
-	},
+    //Return if is a file
+    return stat.isFile();
+  },
 
-	//Check if path is a directory
-	isDir: function(path, callback)
-	{
-		//Get the stat for this path
-		pstat.Stat(path, function(stat)
-		{
-			//Check for error
-			if(stat === false){ return callback(false); }
+  //Check if path is a directory
+  isDir: function(path, callback)
+  {
+    //Get the stat for this path
+    pstat.Stat(path, function(stat)
+    {
+      //Check for error
+      if(stat === false){ return callback(false); }
 
-			//Else, return if is a directory
-			return callback(stat.isDirectory());
-		});
-	},
+      //Else, return if is a directory
+      return callback(stat.isDirectory());
+    });
+  },
 
-	//Check if path is a directory sync
-	isDirSync: function(path)
-	{
-		//Get the stat for this path
-		var stat = pstat.StatSync(path);
+  //Check if path is a directory sync
+  isDirSync: function(path)
+  {
+    //Get the stat for this path
+    var stat = pstat.StatSync(path);
 
-		//Check for error
-		if(stat === false){ return false; }
+    //Check for error
+    if(stat === false){ return false; }
 
-		//Return if is a directory
-		return stat.isDirectory();
-	},
+    //Return if is a directory
+    return stat.isDirectory();
+  },
 
-	//Path stat
-	Stat: function(path, callback)
-	{
-		//Get the path stat
-		fs.stat(path, function(error, stat)
-		{
-			//Check for error
-			if(error){ return callback(false); }
+  //Path stat
+  Stat: function(path, callback)
+  {
+    //Get the path stat
+    fs.stat(path, function(error, stat)
+    {
+      //Check for error
+      if(error){ return callback(false); }
 
-			//Do the callback with the stat
-			return callback(stat);
-		});
-	},
+      //Do the callback with the stat
+      return callback(stat);
+    });
+  },
 
-	//Path stat sync
-	StatSync: function(path)
-	{
-		//Try to run the fs.statSync
-		try
-		{
-			//Return the stat
-			return fs.statSync(path);
-		}
-		catch(e)
-		{
-			//If error, return false
-			return false;
-		}
-	}
+  //Path stat sync
+  StatSync: function(path)
+  {
+    //Try to run the fs.statSync
+    try
+    {
+      //Return the stat
+      return fs.statSync(path);
+    }
+    catch(e)
+    {
+      //If error, return false
+      return false;
+    }
+  }
 };
 
 //Exports to node
